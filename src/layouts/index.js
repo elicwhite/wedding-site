048c90d3460ade619c2bfb538a43f925f827c142
Eli White <github@eli-white.com>
2019-11-10 21:47:24 -0800

    Making the login splash screen image

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import { TransitionProvider, TransitionViews } from 'gatsby-plugin-transitions';
+import { SwitchTransition, CSSTransition } from 'react-transition-group';
 import Header from '../components/header';
 import NavBar from '../components/navbar';
 import './layout.css';
@@ -16,40 +17,61 @@ const Layout = ({ location, children }) => {
     }
   `);
 
+  const isLogin = location.pathname.includes('login');
+
+  const transitionProvider = (
+    <TransitionProvider
+      location={location}
+      enter={{
+        opacity: 0,
+        config: {
+          duration: 500,
+        },
+      }}
+      usual={{
+        opacity: 1,
+      }}
+      leave={{
+        opacity: 0,
+        config: {
+          duration: 250,
+        },
+      }}
+    >
+      <TransitionViews>{children}</TransitionViews>
+    </TransitionProvider>
+  );
+
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <NavBar />
-        <TransitionProvider
-          location={location}
-          enter={{
-            opacity: 0,
-            config: {
-              duration: 500,
-            },
-          }}
-          usual={{
-            opacity: 1,
-          }}
-          leave={{
-            opacity: 0,
-            config: {
-              duration: 250,
-            },
-          }}
+      <SwitchTransition>
+        <CSSTransition
+          key={isLogin ? 'login' : 'auth'}
+          addEndListener={(node, done) =>
+            node.addEventListener('transitionend', done, false)
+          }
+          classNames="fade"
         >
-          <TransitionViews>{children}</TransitionViews>
-        </TransitionProvider>
-      </div>
+          {isLogin ? (
+            transitionProvider
+          ) : (
+            <>
+              <Header siteTitle={data.site.siteMetadata.title} />
+              <div
+                style={{
+                  margin: `0 auto`,
+                  maxWidth: 960,
+                  padding: `0px 1.0875rem 1.45rem`,
+                  paddingTop: 0,
+                }}
+              >
+                <NavBar />
+                {transitionProvider}
+              </div>
+            </>
+          )}
+        </CSSTransition>
+      </SwitchTransition>
     </>
   );
 };
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { navigate } from 'gatsby';
+import { navigate, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
 import { SwitchTransition, CSSTransition } from 'react-transition-group';
 import './login.css';
 
@@ -9,11 +10,39 @@ import SEO from '../components/seo';
 function useInput() {
   const [value, setValue] = useState('');
   const input = (
-    <input type="text" onChange={e => setValue(e.target.value)} value={value} />
+    <input
+      autocapitalize="off"
+      className="password-field"
+      type="text"
+      onChange={e => setValue(e.target.value)}
+      value={value}
+      placeholder="Enter Password"
+    />
   );
 
   return [value, input];
 }
+
+const Image = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "proposal.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1500) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <Img
+      fluid={data.placeholderImage.childImageSharp.fluid}
+      style={{ height: '100vh' }}
+    />
+  );
+};
 
 const LoginPage = () => {
   const [enteredSuccessfully, setEnteredSuccessfully] = useState(false);
@@ -34,18 +63,44 @@ const LoginPage = () => {
 
   return (
     <>
-      <SEO title="Login" />
-      <SwitchTransition>
-        <CSSTransition
-          key={enteredSuccessfully ? 'success' : 'password'}
-          addEndListener={(node, done) =>
-            node.addEventListener('transitionend', done, false)
-          }
-          classNames="fade"
+      <div style={{ position: 'relative' }}>
+        <SEO title="Login" />
+        <Image />
+        <div
+          style={{
+            left: '50%',
+            margin: 'auto',
+            position: 'absolute',
+            top: '70vh',
+            transform: 'translateX(-50%)',
+            fontSize: 30,
+          }}
         >
-          {enteredSuccessfully ? <span>success!</span> : passwordInput}
-        </CSSTransition>
-      </SwitchTransition>
+          <SwitchTransition>
+            <CSSTransition
+              key={enteredSuccessfully ? 'success' : 'password'}
+              addEndListener={(node, done) =>
+                node.addEventListener('transitionend', done, false)
+              }
+              classNames="fade"
+            >
+              {enteredSuccessfully ? (
+                <span
+                  style={{
+                    fontSize: 60,
+                  }}
+                  role="img"
+                  aria-label="success"
+                >
+                  😍
+                </span>
+              ) : (
+                passwordInput
+              )}
+            </CSSTransition>
+          </SwitchTransition>
+        </div>
+      </div>
     </>
   );
 };
