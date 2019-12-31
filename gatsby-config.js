e74e86af4ec77598586761fa757872f131dec529
Eli White <github@eli-white.com>
2019-12-30 23:27:07 -0800

    Update package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  pathPrefix: '/wedding-site',
   siteMetadata: {
     title: `Holly and Eli`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
