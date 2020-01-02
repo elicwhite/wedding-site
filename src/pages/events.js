5b47ea5b59add733f2fe8fe79184292f5556a5f3
Eli White <github@eli-white.com>
2020-01-01 19:12:51 -0800

    Style events page better, wraps on mobile

--- src/pages/events.js
+++ src/pages/events.js
@@ -18,36 +18,61 @@ const EventsPage = () => {
         <div style={styles.flex}>
           <h3 class="cursive">Wedding Ceremony</h3>
           <p>
-            7:00 PM, August 1, 2017
+            4:00 PM, September 12th, 2020
             <br />
-            Golden Gate Park Rose Garden
+            8 N San Pedro St #300
             <br />
-            425 John F Kennedy Dr, San Francisco, CA 94118
+            San Jose, CA 95110
+            <br />
+          </p>
+
+          <h3 class="cursive">After Party</h3>
+          <p>
+            10:00 PM, September 12th, 2020
+            <br />
+            8 N San Pedro St #300
+            <br />
+            San Jose, CA 95110
             <br />
           </p>
         </div>
 
         <div style={styles.flex}>
-          <iframe
-            class="shadow-frame"
-            style={styles.iframe}
-            title="Wedding Event Map"
-            src="https://www.google.com/maps/d/embed?mid=1N3kjqPTb_kVpkz1BPI0tfOOxNIhvRKMq"
-            height="480"
-          ></iframe>
+          <div class="shadow-frame" style={styles.mapContainer}>
+            <div style={styles.mapInnerContainer}>
+              <iframe
+                style={styles.iframe}
+                title="Wedding Event Map"
+                src="https://www.google.com/maps/d/embed?mid=1N3kjqPTb_kVpkz1BPI0tfOOxNIhvRKMq"
+              ></iframe>
+            </div>
+          </div>
         </div>
       </div>
     </>
   );
 };
 
+const MAP_HEADER_HEIGHT = 55;
 const styles = {
   flex: {
     flex: 1,
+    minWidth: '320px',
+  },
+  mapContainer: {
+    overflow: 'hidden',
+    padding: '8px',
+  },
+  mapInnerContainer: {
+    overflow: 'hidden',
+    height: '480px',
   },
   iframe: {
     width: '100%',
+    position: 'relative',
     border: 0,
+    top: -1 * MAP_HEADER_HEIGHT + 'px',
+    height: 480 + MAP_HEADER_HEIGHT + 'px',
   },
 };
 
