7eaa772b4a01dddc30da4c714dc54eb4a8fd225f
Eli White <github@eli-white.com>
2020-01-01 18:08:11 -0800

    Remove additional top margin on header only on mobile

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,16 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import './header.css';
 
 const Header = ({ inline }) => (
   <header
+    className={'header ' + (inline ? 'inline' : '')}
     style={{
       paddingBottom: '30px',
       marginBottom: `1.45rem`,
       textAlign: 'center',
-      ...(inline
-        ? {
-            marginTop: '60px',
-          }
-        : {}),
     }}
   >
-    <div className={inline ? 'inline' : ''}>
+    <div>
       <h1 className="names first">Holly</h1>
       <div className="and">and</div>
       <h1 className="names second">Eli</h1>
