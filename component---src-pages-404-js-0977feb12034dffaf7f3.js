0268b752e4324d9b008d1b26eff197371d9e920d
Eli White <github@eli-white.com>
2020-01-10 22:45:20 -0800

    Updates

---
+++ component---src-pages-404-js-0977feb12034dffaf7f3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),s=a.n(n),c=a("vrFN");t.default=function(){return s.a.createElement("div",{className:"section"},s.a.createElement("div",{className:"section-container"},s.a.createElement(c.a,{title:"404: Not found"}),s.a.createElement("h3",{className:"cursive"},"Oops"),s.a.createElement("p",null,"This page does not exist!")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0977feb12034dffaf7f3.js.map
