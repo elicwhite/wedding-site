19efdbbac3d94e4b2c5abf0b5869d7def1f478da
Eli White <github@eli-white.com>
2021-05-09 23:24:25 -0700

    Update Wedding page and Travel page

--- src/pages/registry.js
+++ src/pages/registry.js
@@ -8,19 +8,37 @@ import { withPrivateRoute } from '../components/privateRoute';
 const SHOW_REGISTRY = false;
 
 const RegistryPage = () => {
-  const content = SHOW_REGISTRY ? <span>Registry!</span> : <ComingSoon />;
-
   return (
     <>
       <SEO title="Registry" />
       <div className="section">
         <div className="section-container">
           <h3 className="cursive">Registry</h3>
-          {content}
+          <div className="caps-subheader accent">Zola Gift Registry</div>
+          <p>
+            Your presence on our special day is the greatest present of all!
+            However, if you do wish to celebrate with a gift, we have registered
+            a list through Zola.
+          </p>
+
+          <a
+            href="https://www.zola.com/registry/hollyandeli2021"
+            className="submit-button"
+            style={styles.viewRegistry}
+          >
+            View Registry
+          </a>
         </div>
       </div>
     </>
   );
+};
+
+const styles = {
+  viewRegistry: {
+    display: 'inline-block',
+    textDecoration: 'none',
+  },
 };
 
 export default withPrivateRoute(RegistryPage);
--- src/pages/wedding.js
+++ src/pages/wedding.js
@@ -1,14 +1,96 @@
 import React from 'react';
 
 import SEO from '../components/seo';
-import ComingSoon from '../components/comingSoon';
+import blancoImage from './blanco.png';
+import Img from 'gatsby-image';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import { withPrivateRoute } from '../components/privateRoute';
 
-const SHOW_WEDDING = false;
+const Invitation = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      invitation: file(relativePath: { eq: "invitation.png" }) {
+        childImageSharp {
+          preview: fluid(maxWidth: 900, quality: 100) {
+            ...GatsbyImageSharpFluid
+          }
+          fullscreen: fluid(maxWidth: 1000, quality: 100) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <div
+      style={{
+        maxWidth: '80%',
+        margin: 'auto',
+      }}
+    >
+      <a
+        href={data.invitation.childImageSharp.fullscreen.src}
+        className="shadow-frame"
+        target="_blank"
+        style={{
+          display: 'block',
+          marginBottom: 0,
+        }}
+      >
+        <Img fluid={data.invitation.childImageSharp.preview} />
+      </a>
+    </div>
+  );
+};
 
 const WeddingPage = () => {
-  const content = SHOW_WEDDING ? (
+  return (
+    <>
+      <SEO title="Wedding" />
+      <div className="section">
+        <div className="section-container">
+          <h3 className="cursive">Venue</h3>
+          <div className="caps-subheader accent">Blanco Urban Venue</div>
+          <img
+            src={blancoImage}
+            style={{
+              width: '50%',
+            }}
+          />
+          <p>
+            Ceremony, cocktail hour, and reception will all be held at Blanco
+            Urban Venue.
+          </p>
+          <p>
+            <a href="https://goo.gl/maps/5Aw9RPZbZWeKLURp7" className="accent">
+              12 N San Pedro St, San Jose, CA 95110
+            </a>
+          </p>
+        </div>
+      </div>
+      <div className="section">
+        <div className="section-container">
+          <Invitation />
+        </div>
+      </div>
+      <div className="section">
+        <div className="section-container">
+          <h3 className="cursive">Wedding</h3>
+          <div className="caps-subheader accent">Coming Soon</div>
+          <p>
+            More details on when doors open and a timeline of events will be
+            available at a later date.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+function OldWeddingLayout() {
+  return (
     <>
       <h3 className="cursive">Ceremony & Reception</h3>
       <div
@@ -41,21 +123,8 @@ const WeddingPage = () => {
         </div>
       </div>
     </>
-  ) : (
-    <>
-      <h3 className="cursive">Wedding</h3>
-      <ComingSoon />
-    </>
   );
-  return (
-    <>
-      <SEO title="Wedding" />
-      <div className="section">
-        <div className="section-container">{content}</div>
-      </div>
-    </>
-  );
-};
+}
 
 const MAP_HEADER_HEIGHT = 55;
 const styles = {
